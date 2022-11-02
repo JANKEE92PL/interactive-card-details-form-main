21c5c50c855b8b8db68d5d59d1920ae71a31e794
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-11-02 12:02:23 +0100

    finish logic in JS for binding data to cc

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ let form = document.forms[0],
     expDateMonth = form.querySelector("#expDateMonth"),
     expDateYear = form.querySelector("#expDateYear"),
     cvc = form.querySelector("#cvc"),
-    submit = form.querySelector("#submit")
+    submit = form.querySelector("#submit"),
+    cardFront = document.querySelector("#bg-card-front-logo"),
+    cardBack = document.querySelector("#csv-cc")
 
 
 console.log(submit)
+
+cardholderName.addEventListener("input", (e) => {
+    cardFront.querySelector("p").innerText = cardholderName.value.toUpperCase()
+});
+
+cardNumber.addEventListener("input", (e) => {
+    cardFront.querySelector("h2").innerText = cardNumber.value
+});
+expDateMonth.addEventListener("input", (e) => {
+    cardFront.querySelectorAll("span")[0].innerText = expDateMonth.value
+});
+expDateYear.addEventListener("input", (e) => {
+    cardFront.querySelectorAll("span")[1].innerText = expDateYear.value
+})
+cvc.addEventListener("input", (e) => {
+    cardBack.innerText = cvc.value
+})
